716bc2803ad67bb03565c2bfa40af61388348e66
RV <rafal.piwnicki@gmail.com>
2018-11-13 17:57:02 +0100

    empty list fixed

--- losowisko1/views.py
+++ losowisko1/views.py
@@ -13,6 +13,14 @@ def wynik(request):
 
     osoby = request.GET.getlist('osoby')
 
+    if not osoby:
+        osoby.append('Ty xD')
+
+    twujstary = ['Dafko', 'Macias', 'Kuba', 'Pszemek', 'Rafas']
+    if osoby == twujstary:
+         osoby.clear()
+         osoby = ['Twuj stary pijany']
+
 
 
     return render(request, 'losowisko1/wynik.html', {'wygrany':(random.choice(osoby))})
